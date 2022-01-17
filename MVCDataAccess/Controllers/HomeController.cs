2cbd22b2e66aa41646dd049eb7683c54ebcdc3c2
vandana l <learnto885@gmail.com>
2022-01-18 00:57:06 +1300

    Add project files.

---
+++ MVCDataAccess/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using DataLibrary.BusinessLogic;
+using MVCDataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using static DataLibrary.BusinessLogic.EmployeeProcessor;
+
+namespace MVCDataAccess.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult ViewEmployees()
+        {
+            ViewBag.Message = "Employees List";
+            var data = LoadEmployees();
+            List<EmployeeModel> employees = new List<EmployeeModel>();
+
+            foreach(var row in data)
+            {
+                employees.Add(new EmployeeModel
+                {
+                    EmployeeId = row.EmployeeId,
+                    FirstName = row.FirstName,
+                    LastName = row.LastName,
+                    EmailAddress = row.EmailAddress,
+                    ConfirmEmail = row.EmailAddress
+                });
+            }
+            return View(employees);
+        }
+
+        public ActionResult SignUp()
+        {
+            ViewBag.Message = "Employee Sign Up";
+
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SignUp(EmployeeModel model)
+        {
+            if (ModelState.IsValid)
+            {
+              int recordsCreated =  CreateEmployee(model.EmployeeId, 
+                    model.FirstName,
+                    model.LastName, 
+                    model.EmailAddress);
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ MVCDataAccess/Models/EmployeeModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCDataAccess.Models
+{
+    public class EmployeeModel
+    {
+        [Display(Name ="Employee ID")]
+        [Range(100000,999999, ErrorMessage = "You need to enter a valid EmployeeId")]
+        public int EmployeeId { get; set; }
+
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "You need to give us your first name.")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "You need to give us your last name.")]
+        public string LastName { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = "You need to give us your email address.")]
+        public string EmailAddress { get; set; }
+
+
+        [Display(Name = "Confirm Email")]
+        [Required(ErrorMessage = "The Email and Confirm Email must match.")]
+        public string ConfirmEmail { get; set; }
+
+
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "You must have a password.")]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength =10, ErrorMessage ="You need to provide a long enough password.")]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Your password and confirm password do not match." )]
+        public string ConfirmPassword { get; set; }
+    }
+}
